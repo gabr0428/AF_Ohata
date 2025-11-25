ea8d3c5dd142e1d2af86ca0d4bad62d98f36f304
gabr0428 <125515330+gabr0428@users.noreply.github.com>
2025-11-24 21:39:56 -0300

    Add files via upload

---
+++ src/TesteCalculadora.java
@@ -0,0 +1,39 @@
+
+
+public class TesteCalculadora {
+
+    public static void main(String[] args) {
+        Calculadora calc = new Calculadora();
+
+        /**
+        *  Chama a função calcular, e insere as variaveis a , b , operador.
+        * isso dentro de um SOUT para já exibir o resultado no console
+        * 
+        */
+
+        System.out.println(calc.calcular( 2, 3, "+"));
+        System.out.println(calc.calcular( 2, 3, "-"));
+        System.out.println(calc.calcular( 2, 3, "*"));
+        System.out.println(calc.calcular( 2, 3, "/"));
+        
+         /**
+        *  Tratamento de exceções (try/catch)
+        * Aqui tentamos executar operações que podem gerar erro,
+        * como dividir por zero ou usar um operador inválido.
+        * Quando o erro é detectado dentro do método calcular,
+        * é lançada uma IllegalArgumentException.
+        * O bloco catch captura o erro e exibe a mensagem da exceção.
+        */
+
+        try {
+            System.out.println(calc.calcular(8, 0, "/")); // Tentativa de divisão por zero → gera exceção
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());// Exibe mensagem da exceção
+        }
+        try {
+            System.out.println(calc.calcular( 5, 5, "x"));  // Operador inválido → gera exceção
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());// Exibe mensagem da exceção
+        }
+    }
+}
